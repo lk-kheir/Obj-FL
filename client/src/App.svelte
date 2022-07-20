f5a0a827f8d9a7baf8a24a7d737a172d23ee1d33
louzanizineddine <louzanizineddine@gmail.com>
2022-07-20 11:55:54 +0100

    connected backend to frontend

---
+++ client/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {onMount} from "svelte";
+    import axios from "axios";
+
+    let objectsLost = [];
+
+    onMount(async ()  => {
+        const {data} = await axios.get('http://localhost:3000/api/objects/object-lost');
+        console.log(data)
+        objectsLost = data
+    })
+
+</script>
+
+<main>
+    {#each objectsLost as objLost}
+        <p>{objLost.dateOfLost}</p>
+        <p>
+            Name: {objLost.loser.name}
+            Level: <span>{objLost.loser.level}</span>
+            Class: <span>{objLost.loser.class}</span>
+        </p>
+
+    {/each}
+</main>
+
+<style>
+
+</style>
